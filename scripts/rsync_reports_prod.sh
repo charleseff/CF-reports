3b3e681048927d3b1474e9e64774af1759348e48
charles.finkel <charles.finkel@6c4f0145-5e20-0410-b8c2-df19fc9885fb>
2007-04-20 18:24:56 +0000

    removed rsync files
    git-svn-id: https://cf-reports.googlecode.com/svn/trunk@3 6c4f0145-5e20-0410-b8c2-df19fc9885fb

--- scripts/rsync_reports_prod.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-# copy script
-
-# CHMOD:
-chmod -R 775 ./reports
-find ./reports -type f -exec chmod 644 {} \;
-
-# RSYNC:
-rsync -vv -e ssh --times --recursive reports/ tomcat@tools1-web:~/reports
--- scripts/rsync_reports_qa.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-# copy script
-
-# CHMOD:
-chmod -R 775 ./reports/dev
-find ./reports/dev -type f -exec chmod 644 {} \;
-
-# RSYNC:
-rsync -vv -e ssh --times --recursive reports/dev/ tomcat@qa:~tomcat/tomcat-5.0.27/reports
